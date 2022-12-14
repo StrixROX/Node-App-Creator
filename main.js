6dd9fd6f3051f0e3320b553582b2cddeaa1a4e5d
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-12-15 01:05:18 +0530

    Added code to ls root and public dir at production server start

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+if(process.env.NODE_ENV === 'production'){
+    const fs = require('fs')
+
+    fs.readdirSync(".").forEach(file => console.log(`/${file}`))
+    console.log()
+    fs.readdirSync("./public").forEach(file => console.log(`/public/${file}`))
+    console.log()
+}
+
 const express = require('express');
 const favicon = require('serve-favicon');
 const router = require('./routes/router');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const source = process.env.NODE_ENV == 'production' ? 'public/dist' : 'public/src';
+const source = process.env.NODE_ENV === 'production' ? 'public/dist' : 'public/src';
 
 app.use(favicon('./assets/favicon.ico'));
 
